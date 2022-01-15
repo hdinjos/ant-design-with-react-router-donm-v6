d4b7c23291a390c8403615fe8b1bcb543a5a4408
hdinjos <hdinjos@gmail.com>
2022-01-15 23:53:24 +0700

    project ini with antd

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import "./assets/style/antd.less"
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
