e238bf19e29f320abeeeb4e55154c4fc100deb3b
hdinjos <hdinjos@gmail.com>
2022-01-17 16:09:24 +0700

    revision dashboard

--- src/views/Layout/BaseLayout.js
+++ src/views/Layout/BaseLayout.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Breadcrumb } from 'antd';
 import {
   MenuUnfoldOutlined,
   MenuFoldOutlined,
@@ -9,7 +9,7 @@ import {
 } from '@ant-design/icons';
 import style from "../../assets/style/BaseLayout/layout.module.css";
 
-const { Header, Sider, Content } = Layout;
+const { Header, Sider, Content, Footer } = Layout;
 
 const BaseLayout = () => {
   const [collapsed, setCollapsed] = useState(false);
@@ -20,8 +20,8 @@ const BaseLayout = () => {
         <div className={style.logo}>
           APP JOS
         </div>
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1" icon={<UserOutlined />}>
+        <Menu theme='dark' mode="inline" defaultSelectedKeys={['1']}>
+          <Menu.Item style={{ marginTop: 0 }} key="1" icon={<UserOutlined />}>
             nav 1
           </Menu.Item>
           <Menu.Item key="2" icon={<VideoCameraOutlined />}>
@@ -30,23 +30,29 @@ const BaseLayout = () => {
           <Menu.Item key="3" icon={<UploadOutlined />}>
             nav 3
           </Menu.Item>
+
         </Menu>
       </Sider>
       <Layout className={style["site-layout"]} style={{ minHeight: "100vh" }}>
-        <Header className={style["site-layout-background"]} style={{ padding: 0 }}>
+        <Header className={style.header}>
           {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
             className: style.trigger,
             onClick: () => setCollapsed(!collapsed),
           })}
         </Header>
+        <Breadcrumb style={{ margin: '80px 16px 0px 16px' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
         <Content
           className={style["site-layout-background"]}
           style={{
-            margin: '24px 16px',
+            margin: '16px 16px 0px 16px',
             padding: 24,
           }}
         >
-          {(() => {
+          {/* {(() => {
             let number = [];
             for (let i = 1; i < 100; i++) {
               number.push(i);
@@ -57,9 +63,10 @@ const BaseLayout = () => {
                 <div key={index}>{num}</div>
               )
             })
-          })()}
+          })()} */}
           Content
         </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
       </Layout>
     </Layout>
   );
